662576f0d1fbd2ce16c8e9799e9e8b62c210f5d0
santosmelqui <melqui.js@gmail.com>
2024-07-11 11:05:09 -0300

    aula 3

---
+++ src/models/repositories/activities_repository.py
@@ -0,0 +1,32 @@
+from typing import Dict, Tuple, List
+from sqlite3 import Connection
+
+class ActivitiesRepository:
+    def __init__(self, conn: Connection) -> None:
+        self.__conn = conn
+
+    def registry_activitiy(self, activity_infos: dict) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+                INSERT INTO activities
+                    (id, trip_id, title, occurs_at)
+                Values
+                    (?, ?, ?)
+            ''', (
+                activity_infos["id"],
+                activity_infos["trip_id"],
+                activity_infos["title"],
+                activity_infos["occurs_at"]
+
+            )
+        )
+        self.__conn.commit()
+
+    def find_acitivities_from_trip(self, trip_id: str) -> List[Tuple]:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''SELECT * FROM activities Where trip_id = ?''', (trip_id,)
+        )
+        activities = cursor.fetchone()
+        return activities
